bfdc485e5eded5d34b863b604e7198a059a12e35
Hisman <hisman.ns@gmail.com>
2018-07-21 22:46:51 +0700

    add config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    networks: [
+        {
+            name: 'Tronscan Mainnet',
+            url: 'https://api.tronscan.org'
+        },
+        {
+            name: 'Tronscan Testnet',
+            url: 'https://testapi.tronscan.org'
+        }
+    ]
+}
